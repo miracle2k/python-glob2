7c14de1e9e8a448e1244f4843362c3e038666ef8
Michael Elsdoerfer <michael@elsdoerfer.com>
2011-04-25 15:46:49 +0200

    Added basic setup.py file.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+# Figure out the version
+import re
+here = os.path.dirname(os.path.abspath(__file__))
+version_re = re.compile(
+    r'__version__ = (\(.*?\))')
+fp = open(os.path.join(here, 'src/glob2', '__init__.py'))
+version = None
+for line in fp:
+    match = version_re.search(line)
+    if match:
+        version = eval(match.group(1))
+        break
+else:
+    raise Exception("Cannot find version in __init__.py")
+fp.close()
+
+
+setup(
+    name = 'glob2',
+    version = ".".join(map(str, version)),
+    description = 'Version of the glob module that can capture.',
+    author = 'Michael Elsdoerfer',
+    author_email = 'michael@elsdoerfer.com',
+    url = 'http://github.com/miracle2k/python-glob2/',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        ],
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+)
