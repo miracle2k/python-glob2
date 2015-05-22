65d0c2d7d1e874dfd26f1eab3e61cbf23ce84fe1
Michael Elsdörfer <michael@elsdoerfer.com>
2015-05-22 22:45:52 +0200

    Remove nosetests dependency.
    You can now use other testing frameworks.

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from os import path
 import shutil
 import tempfile
 
-from nose.tools import assert_equals
-
 import glob2
 from glob2 import fnmatch
 
@@ -91,69 +89,69 @@ class TestRecursive(BaseTest):
 
     def test_recursive(self):
         # ** includes the current directory
-        assert_equals(sorted(glob2.glob('**/*.py', True)), [
+        assert sorted(glob2.glob('**/*.py', True)) == [
             ('a/bar.py', ('a', 'bar')),
             ('a/foo/hello.py', ('a/foo', 'hello')),
             ('b/bar.py', ('b', 'bar')),
             ('file.py', ('', 'file')),
-        ])
+        ]
 
     def test_exclude_root_directory(self):
-        # If files from the rot directory should not be included,
+        # If files from the root directory should not be included,
         # this is the syntax to use:
-        assert_equals(sorted(glob2.glob('*/**/*.py', True)), [
+        assert sorted(glob2.glob('*/**/*.py', True)) == [
             ('a/bar.py', ('a', '', 'bar')),
             ('a/foo/hello.py', ('a', 'foo', 'hello')),
             ('b/bar.py', ('b', '', 'bar'))
-        ])
+        ]
 
     def test_only_directories(self):
         # Return directories only
-        assert_equals(sorted(glob2.glob('**/', True)), [
+        assert sorted(glob2.glob('**/', True)) == [
             ('a/', ('a',)),
             ('a/foo/', ('a/foo',)),
             ('b/', ('b',)),
-        ])
+        ]
 
     def test_parent_dir(self):
         # Make sure ".." can be used
         os.chdir(path.join(self.basedir, 'b'))
-        assert_equals(sorted(glob2.glob('../a/**/*.py', True)), [
+        assert sorted(glob2.glob('../a/**/*.py', True)), [
             ('../a/bar.py', ('', 'bar')),
             ('../a/foo/hello.py', ('foo', 'hello'))
-        ])
+        ]
 
     def test_fixed_basename(self):
-        assert_equals(sorted(glob2.glob('**/bar.py', True)), [
+        assert sorted(glob2.glob('**/bar.py', True)) == [
             ('a/bar.py', ('a',)),
             ('b/bar.py', ('b',)),
-        ])
+        ]
 
     def test_all_files(self):
         # Return all files
         os.chdir(path.join(self.basedir, 'a'))
-        assert_equals(sorted(glob2.glob('**', True)), [
+        assert sorted(glob2.glob('**', True)) == [
             ('bar.py', ('bar.py',)),
             ('foo', ('foo',)),
             ('foo/hello.py', ('foo/hello.py',)),
             ('foo/world.txt', ('foo/world.txt',)),
-        ])
+        ]
 
     def test_root_directory_not_returned(self):
         # Ensure that a certain codepath (when the basename is globbed
         # with ** as opposed to the dirname) does not cause
         # the root directory to be part of the result.
         # -> b/ is NOT in the result!
-        assert_equals(sorted(glob2.glob('b/**', True)), [
+        assert sorted(glob2.glob('b/**', True)) == [
             ('b/bar.py', ('bar.py',)),
             ('b/py', ('py',)),
-        ])
+        ]
 
     def test_non_glob(self):
         # Test without patterns.
-        assert_equals(glob2.glob(__file__, True), [
+        assert glob2.glob(__file__, True) == [
             (__file__, ())
-        ])
-        assert_equals(glob2.glob(__file__), [
+        ]
+        assert glob2.glob(__file__) == [
             (__file__)
-        ])
+        ]
